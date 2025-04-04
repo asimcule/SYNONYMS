488b9db69ece6a850dc7e03e27b0c6fa54e4111d
asimcule <asimcule11@gmail.com>
2025-04-04 14:21:15 -0600

    fixed positioning and added buttons

--- main.js
+++ main.js
@@ -1,20 +1,42 @@
 const displayBar = document.createElement('div');
+const cross_button=document.createElement('span');
 
-function doStyling(rect, selected_text){
-    displayBar.innerText=selected_text;
-    document.body.appendChild(displayBar);  // temporarily load to determine the dimensions
-    const dimension=displayBar.getBoundingClientRect()  // dimensions calculated by the browser
-    document.body.removeChild(displayBar);
+function styleCrossButton(){
+    displayBar.appendChild(cross_button);
+    cross_button.class='close-btn';
+    cross_button.style.position='absolute';
+    cross_button.style.padding="1px 1px 1px 1px"
+    cross_button.style.top='1px';
+    cross_button.style.right='3px';
+    cross_button.innerText='X';
+    cross_button.style.color='white';
+    cross_button.style.cursor='pointer';
+}
+
+function styleContainer(rect){
+    displayBar.class="auto-size";
     displayBar.style.position='absolute';
-    displayBar.style.width = `${dimension.width}px`;;        // this should be dynamic
-    displayBar.style.height = `${dimension.height}px`;
     displayBar.style.zIndex = '1000';
-    displayBar.style.opacity= '0.9';        // fairly opaque but there is some transparency
-    displayBar.style.top = `${rect.top + window.scrollY - (dimension.height+1)}px`; // Position above the selected text
-    displayBar.style.left = `${rect.left + window.scrollX}px`; // Position at the left of the selected text
-    displayBar.style.backgroundColor = 'black';
-    displayBar.style.padding = '5px';
+    displayBar.style.opacity= '0';                  // fix to stop flickering because right now the div is randomly placed on the screen
+    displayBar.style.backgroundColor = 'black';     // this will be dynamically changed based on the theme of the webpage (if black, make it bright)
+    displayBar.style.padding = '15px 15px 15px 15px';
     displayBar.style.color='red';
+
+    // Style and add the cross button to the main container
+    styleCrossButton();
+    document.body.appendChild(displayBar);
+
+    // Correct position calculation of the div and placing it right above the text
+    const dimension = displayBar.getBoundingClientRect();       // this is the dimension of my div determined by the browser
+    displayBar.style.top = `${rect.top + window.scrollY - (dimension.height+10)}px`; // Position above the selected text
+    displayBar.style.left = `${rect.left + window.scrollX}px`; // Position at the left of the selected text
+    displayBar.style.opacity= '0.8';                            // fairly opaque but there is some transparency
+    displayBar.style.fontWeight='800';
+}
+
+function formatText(selected_text){
+    return `${selected_text}\n${selected_text}\n${selected_text}\n${selected_text}\n${selected_text}\n${selected_text}\n`;
+
 }
 
 const handleSelection = () => {
@@ -22,9 +44,9 @@ const handleSelection = () => {
     if (selected_text.length > 0) {
         // Creating a new div object displayBar        
         const rect = window.getSelection().getRangeAt(0).getBoundingClientRect();
-        doStyling(rect, selected_text);
-        displayBar.innerText=selected_text;
-        document.body.appendChild(displayBar);
+        const displayText=formatText(selected_text);
+        displayBar.innerText=displayText;
+        styleContainer(rect);
     };
 };
 
@@ -33,10 +55,11 @@ const handleClick=()=>{
 };
 
 document.addEventListener('mouseup', handleSelection);
+cross_button.addEventListener('click', ()=>{
+    displayBar.remove();
+});
+
 // document.addEventListener('click', handleClick);
 // Ideas:
-// Display bar position needs to be rethought
-// remove the popup on double click or a single click outside the box
-// Dynamically resizing the pop up window if there is a long list of synonyms
 // Based on the background of the webpage, dynamically change the color of the popup to make it more appealing 
 
