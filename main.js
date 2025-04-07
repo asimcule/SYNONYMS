d77568f7af4db4bc4630959d612aed843276995d
asimcule <92946936+asimcule@users.noreply.github.com>
2025-04-06 22:26:44 -0600

    Update main.js
    Api call to display synonyms

--- main.js
+++ main.js
@@ -30,21 +30,39 @@ function styleContainer(rect){
     const dimension = displayBar.getBoundingClientRect();       // this is the dimension of my div determined by the browser
     displayBar.style.top = `${rect.top + window.scrollY - (dimension.height+10)}px`; // Position above the selected text
     displayBar.style.left = `${rect.left + window.scrollX}px`; // Position at the left of the selected text
-    displayBar.style.opacity= '0.8';                            // fairly opaque but there is some transparency
+    displayBar.style.opacity= '0.9';                            // fairly opaque but there is some transparency
     displayBar.style.fontWeight='800';
 }
 
-function formatText(selected_text){
-    return `${selected_text}\n${selected_text}\n${selected_text}\n${selected_text}\n${selected_text}\n${selected_text}\n`;
+function formatText(json_data){
+    var result=""
+    for (let i=0; i<json_data.length; i++){
+       result+=json_data[i]["word"]+"\n";
+    }
+    return result||"No synonyms found. Please try selecting a noun"
 
 }
 
-const handleSelection = () => {
+
+async function retrieveSynonyms(word){
+    try{
+        const response=await fetch(`https://api.datamuse.com/words?rel_syn=${word}`);
+        const data = await response.json();
+        console.log(data);
+        return data;
+    } catch(err){
+        console.error('fetch error:', err);
+    }
+
+}
+
+const handleSelection = async () => {
     const selected_text = window.getSelection().toString().trim();
     if (selected_text.length > 0) {
         // Creating a new div object displayBar        
         const rect = window.getSelection().getRangeAt(0).getBoundingClientRect();
-        const displayText=formatText(selected_text);
+        const synonyms=await retrieveSynonyms(selected_text);       // this has to be wrapped in async also
+        const displayText=formatText(synonyms);
         displayBar.innerText=displayText;
         styleContainer(rect);
     };
@@ -62,4 +80,3 @@ cross_button.addEventListener('click', ()=>{
 // document.addEventListener('click', handleClick);
 // Ideas:
 // Based on the background of the webpage, dynamically change the color of the popup to make it more appealing 
-
